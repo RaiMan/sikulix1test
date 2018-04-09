6e300b26d650cbc28ef0d855652a4ee53f9faea3
RaiMan <rmhdevelop@me.com>
2018-04-09 14:53:19 +0200

    added: org.sikuli.script API

--- src/test/java/sikulix/com/sikulix1test/TestAll.java
+++ src/test/java/sikulix/com/sikulix1test/TestAll.java
@@ -2,6 +2,7 @@ package sikulix.com.sikulix1test;
 
 import org.junit.*;
 import org.junit.runners.MethodSorters;
+import org.sikuli.script.Screen;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestAll {
@@ -9,6 +10,8 @@ public class TestAll {
   private static void log(String message, Object... args) {
     System.out.println(String.format("[TestAll] " + message, args));
   }
+
+  static Screen scr = new Screen();
 
   @BeforeClass
   public static void setUpClass() {
@@ -35,6 +38,7 @@ public class TestAll {
   @Test
   public void test_000_play() {
     currentTest = "test_000_play";
+    log("Screen: %s", scr);
     result = "should not fail ;-)";
     assert true;
   }
