f52d8abc60354f83cf95f9374ea7c043e0047ef7
RaiMan <rmhdevelop@me.com>
2019-10-25 07:45:09 +0200

    version 2.0.0

--- src/main/java/com/sikulix/sikulix1test/Sikulix1Test.java
+++ src/main/java/com/sikulix/sikulix1test/Sikulix1Test.java
@@ -1,7 +1,34 @@
 package com.sikulix.sikulix1test;
 
+import org.sikuli.basics.Debug;
+import org.sikuli.basics.Settings;
+import org.sikuli.script.Screen;
+
 public class Sikulix1Test {
   public static void main(String[] args) {
-    System.out.println("Sikulix1Test: nothing to do here");
+    boolean shouldEval = false;
+    if (args.length > 0) {
+      for (String arg : args) {
+        if ("eval".equals(arg)) {
+          shouldEval = true;
+          break;
+        }
+      }
+      if (shouldEval) {
+        Debug.logp("SikulixAPI: running eval");
+        //Settings.DebugLogs = true;
+        Settings.LogTime = true;
+        Debug.on(3);
+        Debug.log(3, "Debug *****");
+        Debug.user("userlog *****");
+        new Screen().click();
+        Debug.logp("SikulixAPI: ending eval");
+        //Debug.off();
+      }
+      //Sikulix.terminate();
+    }
+    if (!shouldEval) {
+      System.out.println("Sikulix1Test: nothing to do here");
+    }
   }
 }
